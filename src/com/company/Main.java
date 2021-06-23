a5835281e19a9a89cb12891bf839b0ea1cad5dce
overflowhidden <overflowhidden@gmail.com>
2021-06-23 21:12:40 +0300

    Hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+		s123(100);
+	}
+
+    static boolean s123(int a){
+
+	}
+
+	static int sum(int a, int b, int c){
+		int d = a + b + c;
+		return c;
+	}
+
+	static void dif(int a, int b){
+		int c = a%b;
+		System.out.println(c);
+	}
+
+	static void mul(int a, int b){
+		int c = a * b;
+		System.out.println(c);
+	}
+
+	static void div(int a, int b){
+		int c = a/b;
+		System.out.println(c);
+	}
+
+}
